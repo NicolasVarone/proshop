c9e11b0f5697dbccdbffcfaac545e11230887482
nicolas varone <varonenico@gmail.com>
2021-07-01 21:40:54 +0200

    Create place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
-import Footer from './components/Footer'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -18,8 +19,9 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/payment' component={PaymentScreen}></Route>
           <Route path='/shipping' component={ShippingScreen}></Route>
+          <Route path='/payment' component={PaymentScreen}></Route>
+          <Route path='/placeorder' component={PlaceOrderScreen}></Route>
           <Route path='/login' component={LoginScreen}></Route>
           <Route path='/register' component={RegisterScreen}></Route>
           <Route path='/profile' component={ProfileScreen}></Route>
